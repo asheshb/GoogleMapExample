018e990871f3604c4f2b0a49ea76b11b0fcd8645
Ashesh Bharadwaj <ppashesh@gmail.com>
2020-02-08 12:40:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/googlemapexample/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.googlemapexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) { // Add a marker in Sydney, Australia,
+        val newDelhi = LatLng(28.629717, 77.207065)
+        googleMap.addMarker(
+            MarkerOptions().position(newDelhi)
+                .title("Marker at New Delhi")
+        )
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(newDelhi))
+    }
+}
